af730386099dc5d3a71ad176a63b776e4b1332fa
Jake <kaelscion@gmail.com>
2016-12-04 21:48:01 +0000

    Removed send_email.py and added a function to download the images as files instead.

--- mine_and_output.py
+++ mine_and_output.py
@@ -1,21 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
+import urllib2
+import shutil
+
+urls = []
 
 class data_miner:
-
+    
 
     def __init__(self, base_url):
         self.base_url = base_url
 
 
-    def get_links(self):
+    def get_links(self, query):
         html=self.base_url
-        payload={'q':'fluffy kittens'}
+        payload={'q': query}
         r=requests.get(html, params=payload)
         soup=BeautifulSoup(r.content, "html5lib")
-        with open('image_results.html', 'w') as f:
-            for image in soup.find_all('img'):
-                f.write(image.attrs['src'] + '\n')
-        
+        for image in soup.find_all('img'):
+            urls.append(image.attrs['src'])
+    
+    def download_files(self, query):
+        number = 1
+        for item in urls:
+            with open(query + str(number) + '.jpg', 'wb') as out_file:
+                req = requests.get(item, stream=True)
+                shutil.copyfileobj(req.raw, out_file)
+                number += 1
+            print number
+                
+    
 x = data_miner('http://www.google.com/images')
-x.get_links()
+x.get_links("fluffy kittens")
+x.download_files("fluffy kittens")
+            
+                
