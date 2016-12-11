2e1057322e82e9c779834da5f30e9fd6b43ddefa
Kaelscion <jake@artsydev.com>
2016-12-11 17:21:59 -0500

    initial commit of refactor branch

---
+++ README.md
@@ -0,0 +1,8 @@
+# open-happiness
+Bring happiness to folks on a daily basis
+
+An attempt to bring a little positivity to people on a daily basis. Open Happiness is intended as a service that will generate 
+an image search query for something positive by randomly drawing keywords from several database tables (think madlibs), then 
+submitting this query to sites such as Google Images and Youtube and downloading the results for display on a continuous-scroll 
+web page. All content will be acquired and presented with the intent of brightening and/or turning around a bad day through 
+positive or humorous imagery!
--- mine_and_output.py
+++ mine_and_output.py
@@ -3,7 +3,11 @@ import requests
 import shutil
 
 urls = []
+<<<<<<< HEAD
 query = 'katmandu'
+=======
+query = 'puppies'
+>>>>>>> d0f0f74a433dd16a2e47f2b50cda38bc4f0f1490
 
 class data_miner:
 
@@ -18,6 +22,7 @@ class data_miner:
         soup=BeautifulSoup(r.content, "html5lib")
         for image in soup.find_all('img'):
             urls.append(image.attrs['src'])
+<<<<<<< HEAD
 
     def download_files(self, query):
         number = 1
@@ -30,3 +35,19 @@ class data_miner:
 x = data_miner('http://www.google.com/images')
 x.get_links(query)
 x.download_files(query)
+=======
+    
+    def download_files(self, query):
+        number = 1
+        for item in urls:
+            with open('static/happy-images/' + query + str(number) + '.jpg', 'wb') as out_file:
+                req = requests.get(item, stream=True)
+                shutil.copyfileobj(req.raw, out_file)
+                number += 1
+    
+x = data_miner('http://www.google.com/images')
+x.get_links(query)
+x.download_files(query)
+            
+                
+>>>>>>> d0f0f74a433dd16a2e47f2b50cda38bc4f0f1490
